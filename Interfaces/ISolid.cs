617c9994f24e8e218f9d88ac35868b7dab058cee
rcladmin <rayconsulting@hotmail.com>
2020-01-17 18:01:21 -0400

    recommended changes to project classes

---
+++ Interfaces/ISolid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1
+{
+    public interface ISolid
+    {
+        IShape Shape { get; set; }
+        int Height { get; set; }
+        public int Volume();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,83 +1,57 @@
 ﻿using System;
 
-namespace Project1{
-  class Program{
-    static void Main(string[] args){
-     while (true){
-      Console.WriteLine("Please enter a watertank name cube/cuboid/cylinder:");
-      string tank = Console.ReadLine();
-      if (tank == "cube"){
-         Console.WriteLine("Please enter the side of a cube:");
-         int side = Convert.ToInt32(Console.ReadLine());
-         cube c = new cube(side);
-         c.volume();
-      }
-      else if (tank == "cuboid"){
-              Console.WriteLine("Please enter the length of a cuboid:");
-              int lenght = Convert.ToInt32(Console.ReadLine());
-              Console.WriteLine("Please enter the breath of a cuboid:");
-              int breath = Convert.ToInt32(Console.ReadLine());
-              Console.WriteLine("Please enter the height of a cuboid:");
-              int height = Convert.ToInt32(Console.ReadLine());
-              cuboid cu = new cuboid(lenght, breath, height);
-              cu.volume();
-      }
-      else if (tank == "cylinder"){
-              Console.WriteLine("Please enter the radius of a cylinder:");
-              double radius = Convert.ToDouble(Console.ReadLine());
-              Console.WriteLine("Please enter the height of a cylinder:");
-              double height = Convert.ToDouble(Console.ReadLine());
-              cylinder cy = new cylinder(radius, height);
-              cy.volume();
-      }
-      else{
-          Console.WriteLine("Please enter a valid option");
-      }
-     }
-     Console.ReadKey();
+namespace Project1
+{
+    class Program
+    {
+        static readonly IVolumeService _volumeService;
+        static ISolid _solid;
+
+        static Program()
+        {
+            /// Inject the VolumeService using the Dependency Inversion Principle
+            _volumeService = new VolumeService();
+        }
+
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine($"{Environment.NewLine}Please enter a watertank solid shape : cube, cuboid or cylinder");
+                string tank = Console.ReadLine();
+
+                if (tank == "cube")
+                {
+                    Console.WriteLine("Please enter the side of the cube:");
+                    int side = Convert.ToInt32(Console.ReadLine());
+                    _solid = new Solid(new Rectangle(side, side), side);
+                }
+                else if (tank == "cuboid")
+                {
+                    Console.WriteLine("Please enter the length of the cuboid:");
+                    int lenght = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter the breath of the cuboid:");
+                    int breadth = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter the height of the cuboid:");
+                    int height = Convert.ToInt32(Console.ReadLine());
+                    _solid = new Solid(new Rectangle(lenght, breadth), height);
+                }
+                else if (tank == "cylinder")
+                {
+                    Console.WriteLine("Please enter the radius of the cylinder:");
+                    int radius = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter the height of the cylinder:");
+                    int height = Convert.ToInt32(Console.ReadLine());
+                    _solid = new Solid(new Circle(radius), height);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid option");
+                }
+
+                Console.WriteLine($"The volume of the watertank is:{_volumeService.Volume(_solid)}");
+            }
+        }
     }
-    public interface watertank{
-       public void volume();
-    }
-    public class cube : watertank{
-       private int side;
-       public cube(int s){
-         side=s;
-       }
-       public void volume(){
-         int vol = side*side*side;
-         Console.WriteLine("The volume of the cube is:{0}",vol);
-         Console.WriteLine("");
-       }
-    }
-    public class cuboid : watertank{
-       private int length;
-       private int breath;
-       private int height;
-       public cuboid(int l,int b,int h){
-         length=l;
-         breath=b;
-         height=h;
-       }
-       public void volume(){
-         int vol = length*breath*height;
-         Console.WriteLine("The volume of the cuboid is:{0}",vol);
-         Console.WriteLine("");
-       }
-    }
-    public class cylinder : watertank{
-       private double radius;
-       private double height;
-       double pi=3.14;
-       public cylinder(double r,double h){
-         radius=r;
-         height=h;
-       }
-       public void volume(){
-         double vol = pi*radius*radius*height;
-         Console.WriteLine("The volume of the cylinder is:{0}",vol);
-         Console.WriteLine("");
-       }
-    }
-  }
 }
